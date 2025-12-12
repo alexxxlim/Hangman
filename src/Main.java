ccddee3aecc4f0ec90260b7bd6527950c50cbe62
alexxxlim <22alex.lim.work22@gmail.com>
2025-12-12 01:18:09 +0100

    new static methods added
    loadWords(), guessWord(), cleanConsole()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 
 //Hangman - Main
 //точка исполнения
@@ -7,9 +11,10 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        File wordsFile = new File("resources/words.txt");
+        loadWords(wordsFile);
         mainMenu();
     }
-
 
     public static void mainMenu() {
         Scanner scanner = new Scanner(System.in);
@@ -37,9 +42,34 @@ public class Main {
         } while (selection != 0);
     }
 
-    //TODO обработка не найденного файла!!!
-    public static String guessWord(File words) {
-        //TODO заглушка ретерна
-        return "";
+    public static ArrayList<String> loadWords(File wordsFile) {
+        ArrayList<String> wordList = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(wordsFile.getAbsolutePath()))) {
+            //TODO поменять на баффер ридер
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                wordList.add(line);
+            }
+            System.out.println("Слова успешно загружены. Можно играть!\n");
+        } catch (FileNotFoundException e) {
+            System.out.println("...Error. Файл со словами не найден...");
+        }
+
+        return wordList;
+    }
+
+    public static String guessWord(ArrayList<String> wordsList) {
+        Random rnd = new Random();
+        int guessedWordIdx = rnd.nextInt(wordsList.size());
+        String guessedWord = wordsList.get(guessedWordIdx);
+
+        return guessedWord;
+    }
+
+    public static void cleanConsole(){
+        for (int i = 0; i < 30; i++) {
+            System.out.println();
+        }
     }
 }
